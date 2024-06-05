4ee2a518e234ae0d46548da8caca9fb065b658ae
Ajo-shunyaekai <Ajo@shunyaekai.tech>
2024-06-05 12:43:43 +0530

    added signup login files

--- src/App.js
+++ src/App.js
@@ -36,24 +36,15 @@ import InvoiceDesign from './components/pay/invoiceDesign.js';
 import CustomModal from './components/pay/CustomModal.js';
 import InvoiceCardDesign from './components/pay/InvoiceCardDesign.js'
 import ProductDetailsCard from './components/ProductDetailsCard.js';
+import SignUp from './signup/SignUp.js';
+import ImageUploader from './signup/ImageUploader.js';
+import SuccessModal from './signup/SuccessModal.js';
+import Login from './signup/Login.js';
 
-
-function App() {
-
-useEffect(() => {
-  sessionStorage.setItem('buyer_name','Pranav')
-  sessionStorage.setItem('buyer_id','BUY-jmn98sdanx')
-  sessionStorage.setItem('buyer_email','buyer1@gmail.org')
-  sessionStorage.setItem('buyer_mobile','8129197512')
-  sessionStorage.setItem('buyer_token','eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0aW1lIjoiTW9uIEFwciAxNSAyMDI0IDEyOjQ5OjM0IEdNVCswNTMwIChJbmRpYSBTdGFuZGFyZCBUaW1lKSIsImJ1eWVySWQiOiJCVVktam1uOThzZGFueCIsImlhdCI6MTcxMzE2NTU3NH0.HvJHPx6WA09qcMedTVVeTpuqlsXOSfTDEjWlU7v1kJQ')
-
-},[])
-
+function IncludeSidebar() {
   return (
-    <>
-      <div className='App'>
-        <Router>
-          <Sidebar>
+    <div>
+       <Sidebar>
             <Routes>
               <Route path="/" element={<Dashboard />} />
               <Route path="/order" element={<Order />} />
@@ -86,7 +77,65 @@ useEffect(() => {
               <Route path="/invoice-card-design" element={<InvoiceCardDesign />} />
               <Route path="/product-details-card" element={<ProductDetailsCard />} />
             </Routes>
-          </Sidebar>
+       </Sidebar>
+    </div>
+  )
+}
+
+function App() {
+
+useEffect(() => {
+  sessionStorage.setItem('buyer_name','Pranav')
+  sessionStorage.setItem('buyer_id','BUY-jmn98sdanx')
+  sessionStorage.setItem('buyer_email','buyer1@gmail.org')
+  sessionStorage.setItem('buyer_mobile','8129197512')
+  sessionStorage.setItem('buyer_token','eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0aW1lIjoiTW9uIEFwciAxNSAyMDI0IDEyOjQ5OjM0IEdNVCswNTMwIChJbmRpYSBTdGFuZGFyZCBUaW1lKSIsImJ1eWVySWQiOiJCVVktam1uOThzZGFueCIsImlhdCI6MTcxMzE2NTU3NH0.HvJHPx6WA09qcMedTVVeTpuqlsXOSfTDEjWlU7v1kJQ')
+
+},[])
+
+  return (
+    <>
+      <div className='App'>
+        <Router>
+          <Routes>
+          <Route path='/*' element={<IncludeSidebar />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/login" element={<Login />} />
+          </Routes>
+          {/* <Sidebar>
+            <Routes>
+              <Route path="/" element={<Dashboard />} />
+              <Route path="/order" element={<Order />} />
+              <Route path="/buy" element={<Buy />} />
+              <Route path="/my-supplier" element={<MySupplier />} />
+              <Route path="/invoice" element={<Invoice />} />
+              <Route path="/support" element={<Support />} />
+              <Route path="/active-order" element={<ActiveOrder />} />
+              <Route path="/complete-order" element={<CompleteOrder />} />
+              <Route path="/deleted-order" element={<DeletedOrder />} />
+              <Route path="/popup-Modal" element={<PopupModal />} />
+              <Route path="/ordercancel" element={<OrderCancel />} />
+              <Route path="/line-chart" element={<LineChart />} />
+              <Route path="/progress-bar" element={<ProgressBar />} />
+              <Route path="/order-details/:orderId" element={<OrderDetails />} />
+              <Route path="/pending-details/:orderId" element={<PendingDetails />} />
+              <Route path="/medicine-details/:medicineId" element={<ProductDetails />} />
+              <Route path="/supplier-details/:supplierId" element={<SupplierDetails />} />
+              <Route path="/completed-orders" element={<CompletedOrders />} />
+              <Route path="/ongoing-orders" element={<OngoingOrders />} />
+              <Route path="/supply-order-list" element={<SupplyOrderList />} />
+              <Route path="/pending-orders" element={<PendingOrders />} />
+              <Route path="/order-details-circular-bar" element={<OrderDetailsCircularBar />} />
+              <Route path="/complete-invoice" element={<CompleteInvoice />} />
+              <Route path="/ongoing-invoice" element={<OngoingInvoice />} />
+              <Route path="/pending-invoice" element={<PendingInvoice />} />
+              <Route path="/upload-document" element={<UploadDocument />} />
+              <Route path="/invoice-design/:orderId" element={<InvoiceDesign />} />
+              <Route path="/custom-modal" element={<CustomModal />} />
+              <Route path="/invoice-card-design" element={<InvoiceCardDesign />} />
+              <Route path="/product-details-card" element={<ProductDetailsCard />} />
+            </Routes>
+          </Sidebar> */}
         </Router>
       </div>
     </>
